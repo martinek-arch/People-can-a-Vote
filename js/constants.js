e84b0f3da070501afdf2acf334da274eeca21a4b
martinek-arch <mar.ti.nek@email.cz>
2026-02-17 20:46:47 +0100

    Add initial i18n module and language switcher

---
+++ js/constants.js
@@ -0,0 +1,4 @@
+export const SUPABASE_URL = "https://jqoomnhpyuikbntnrukw.supabase.co";
+export const SUPABASE_ANON_KEY = "sb_publishable_SjZ_HQWN0XRE9ebbf_OwQg_kmJeS43h";
+export const APP_BASE_URL = "https://martinek-arch.github.io/People-can-a-Vote/";
+export const MAPBOX_TOKEN = "pk.eyJ1IjoiaGVyZ290dCIsImEiOiJjbWttdHQ3cmYwZ2hmM2pyMjd2bmk1c3ZmIn0.-9CViuw1h3-gy49EsKvFiQ";
